3b5166ccf9e0c1b565ee525d5d7d1b2f9d12846c
Devvrat Danu <44670306+Devvratdanu@users.noreply.github.com>
2025-07-08 18:40:45 +0530

    Add files via upload

---
+++ Task1_Basic Mathematical Operations.py
@@ -0,0 +1,10 @@
+First_Number=int(input("Enter the first number: "))
+Second_Number=int(input("Enter the second number: "))
+print("Addition:", First_Number + Second_Number)
+print("Subtraction:", First_Number - Second_Number)
+print("Multiplication:", First_Number * Second_Number)
+print("Division:", First_Number / Second_Number)
+
+"""This code prompts the user for two numbers and performs basic 
+mathematical operations: addition, subtraction, multiplication, and division. 
+It then prints the results of these operations."""
---
+++ Task2_Personalized Greeting.py
@@ -0,0 +1,4 @@
+First_Name=input("Enter your first name: ")
+Last_Name=input("Enter your last name: ")
+print("Hello,", First_Name, Last_Name + "!"+" Welcome to the Python program.")
+# This code prompts the user for their first and last name, then prints a personalized greeting.
