3934ad37107cf5cf161f63a57a98435953f6985e
Devvrat Danu <44670306+Devvratdanu@users.noreply.github.com>
2025-07-08 19:03:31 +0530

    Add files via upload

---
+++ Assignmrn2_task1_Number is Even or Odd.py
@@ -0,0 +1,6 @@
+Number=int(input("Enter a number: "))
+if Number % 2 == 0:
+    print(Number,"is an Even number.")
+else:
+     print(Number,"is an odd number.")
+# This code prompts the user for a number and checks if it is even or odd.
---
+++ Assignmrn2_task2_ Sum of Integers from 1 to 50 Using a Loop.py
@@ -0,0 +1,5 @@
+ADD=0
+for  i in  range (1, 51):
+    ADD+=i
+print("The sum of numbers from 1 to 50 is:", ADD)
+# This code calculates the sum of integers from 1 to 50 using a loop.
